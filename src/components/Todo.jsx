1ba6d1807e7431d352100ac596cf31e07ded7cd0
sai shravan <saishravan384@gmail.com>
2025-01-17 15:34:48 +0530

    feat: format deadline display in Todo component and adjust styling

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -20,6 +20,17 @@ export default function Todo({
   onEdit,
   handleDelete,
 }) {
+  const formatDeadline = (deadline) => {
+    const options = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+    }
+    return new Date(deadline).toLocaleDateString(undefined, options)
+  }
+
   return (
     <article className='todo scale-in-center'>
       <p className='title'>{title}</p>
@@ -37,7 +48,7 @@ export default function Todo({
           >
             <path d='m612-292 56-56-148-148v-184h-80v216l172 172ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-400Zm0 320q133 0 226.5-93.5T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160Z' />
           </svg>
-          <span>{deadline}</span>
+          <span>{formatDeadline(deadline)}</span>
         </div>
       )}
 
