70a8a1c36be415f32359eca9fccabea9a28dd64d
sai shravan <saishravan384@gmail.com>
2025-01-18 14:44:16 +0530

    feat: implement signup form with state management and API integration using axios

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,15 +1,67 @@
 import '../styles/LandingForms.css'
-import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 export default function Signup() {
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+    passwordConfirm: '',
+  })
+  const [isLoading, setIsLoading] = useState(false)
+  const [errors, setErrors] = useState([])
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setUser((prevUser) => ({
+      ...prevUser,
+      [name]: value,
+    }))
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      setIsLoading(true)
+      const response = await axios.post(
+        'http://localhost:3000/users/signup',
+        user
+      )
+
+      localStorage.setItem('token', response.data.token)
+      localStorage.setItem('uid', response.data.uid)
+      navigate('/goals')
+    } catch (error) {
+      setErrors(error.response.data)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  const getErrorMessage = (field) => {
+    const error = errors.find((error) => error.path === field)
+    return error ? error.msg : ''
+  }
+
   return (
     <main className='signup'>
-      <form className='scale-in-center'>
-        <h1>Create your acccount</h1>
+      <form className='scale-in-center' onSubmit={handleSubmit}>
+        <h1>Create your account</h1>
         <div className='formControl'>
           <label htmlFor='email'>Email*:</label>
-          <input type='email' id='email' name='email' required />
-          <span className='error'></span>
+          <input
+            type='email'
+            id='email'
+            name='email'
+            value={user.email}
+            onChange={handleChange}
+            required
+          />
+          {getErrorMessage('email') && (
+            <span className='error'>{getErrorMessage('email')}</span>
+          )}
         </div>
 
         <div className='formControl'>
@@ -18,10 +70,14 @@ export default function Signup() {
             type='password'
             id='password'
             name='password'
+            value={user.password}
+            onChange={handleChange}
             required
             minLength={8}
           />
-          <span className='error'></span>
+          {getErrorMessage('password') && (
+            <span className='error'>{getErrorMessage('password')}</span>
+          )}
         </div>
 
         <div className='formControl'>
@@ -30,14 +86,18 @@ export default function Signup() {
             type='password'
             id='passwordConfirm'
             name='passwordConfirm'
+            value={user.passwordConfirm}
+            onChange={handleChange}
             required
             minLength={8}
           />
-          <span className='error'></span>
+          {getErrorMessage('passwordConfirm') && (
+            <span className='error'>{getErrorMessage('passwordConfirm')}</span>
+          )}
         </div>
 
         <button type='submit' className='filled'>
-          Sign Up
+          {isLoading ? 'Loading...' : 'Sign Up'}
         </button>
         <p>
           Already have an account? <Link to={'/signin'}>Signin</Link>
