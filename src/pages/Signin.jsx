946583514ad39432b0a641c3be53c33587686f43
sai shravan <saishravan384@gmail.com>
2025-01-18 14:50:29 +0530

    feat: implement signin form with state management, API integration, and error handling

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,15 +1,66 @@
 import '../styles/LandingForms.css'
-import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 export default function Signin() {
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  })
+  const [isLoading, setIsLoading] = useState(false)
+  const [errors, setErrors] = useState([])
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setUser((prevUser) => ({
+      ...prevUser,
+      [name]: value,
+    }))
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      setIsLoading(true)
+      const response = await axios.post(
+        'http://localhost:3000/users/signin',
+        user
+      )
+
+      localStorage.setItem('token', response.data.token)
+      localStorage.setItem('uid', response.data.uid)
+      navigate('/goals')
+    } catch (error) {
+      setErrors(error.response.data)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  const getErrorMessage = (field) => {
+    const error = errors.find((error) => error.path === field)
+    return error ? error.msg : ''
+  }
+
   return (
-    <main className='signin '>
-      <form className='scale-in-center'>
+    <main className='signin'>
+      <form className='scale-in-center' onSubmit={handleSubmit}>
         <h1>Login to your account.</h1>
         <div className='formControl'>
           <label htmlFor='email'>Email:</label>
-          <input type='email' name='email' id='email' required />
-          <span className='error'></span>
+          <input
+            type='email'
+            name='email'
+            id='email'
+            value={user.email}
+            onChange={handleChange}
+            required
+          />
+          {getErrorMessage('email') && (
+            <span className='error'>{getErrorMessage('email')}</span>
+          )}
         </div>
 
         <div className='formControl'>
@@ -18,18 +69,18 @@ export default function Signin() {
             type='password'
             name='password'
             id='password'
+            value={user.password}
+            onChange={handleChange}
             required
             minLength={8}
           />
-          <span className='error'></span>
+          {getErrorMessage('password') && (
+            <span className='error'>{getErrorMessage('password')}</span>
+          )}
         </div>
 
-        <button
-          type='submit'
-          className='filled
-        '
-        >
-          Sign in
+        <button type='submit' className='filled'>
+          {isLoading ? 'Loading...' : 'Sign in'}
         </button>
 
         <p>
